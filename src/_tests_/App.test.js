b9f02cb1c6f4bc39ef69f10c7b8406a338d22eed
Bluette1 <marylene.sawyer@gmail.com>
2021-01-22 11:49:04 +0200

    Fix failing ci tests

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import {
+  render, screen, cleanup, waitFor,
+} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import axios from 'axios';
 import App from '../components/App';
+import configureStore, { history } from '../store';
+import { httpProtocol, host, port } from '../envVariables';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+afterEach(cleanup);
+
+const store = configureStore();
+
+const AppWithStore = () => (
+  <Provider store={store}>
+    <React.StrictMode>
+      <ConnectedRouter history={history}><App /></ConnectedRouter>
+    </React.StrictMode>
+  </Provider>
+);
+test('renders the app', async () => {
+  axios.get.mockImplementation(url => {
+    switch (url) {
+      case `${httpProtocol}://${host}:${port}/categories`:
+        return Promise.resolve({ data: { categories: [] } });
+      default:
+        return Promise.resolve({ data: { holiday_homes: [] } });
+    }
+  });
+  render(<AppWithStore />);
+  await waitFor(() => {
+    const holidayHomesTitle = screen.getByText(/Holiday-Homes/i);
+    expect(holidayHomesTitle).toBeInTheDocument();
+  });
 });
