42abded44cec6ad3d65b1481be059fc929e61ace
Bluette1 <marylene.sawyer@gmail.com>
2021-01-23 06:51:39 +0200

    Add logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -5,20 +7,22 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import CategoryFilter from './CategoryFilter';
+import { logout } from '../actions/index';
 
-const Header = () => (
+const Header = ({ logout, user }) => (
+
   <Navbar bg="light" expand="lg">
     <Navbar.Brand href="#home">Holiday-Homes</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link>
           <CategoryFilter />
         </Nav.Link>
-        <NavDropdown title="Username" id="basic-nav-dropdown">
+        <NavDropdown title={user.username} id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
+          <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">Favourites</NavDropdown.Item>
         </NavDropdown>
@@ -31,4 +35,9 @@ const Header = () => (
   </Navbar>
 );
 
-export default Header;
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  user: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default connect(state => ({ user: state.user }), { logout })(Header);
